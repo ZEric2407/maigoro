fe2a96534453eb3c302fbc8dbb5f1e5e3db89c6a
Frank Lin <165003762+SteamyFuton@users.noreply.github.com>
2024-11-23 17:34:48 -0500

    fix minor

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -17,7 +17,7 @@
 	<h2 class="mb-8 text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white">
 		Frequently Asked Questions (FAQ)
 	</h2>
-	<Faq>
+	
 		<div>
 			<FaqItem>
 				<svelte:fragment slot="h3">What does "Maigorou" mean?</svelte:fragment>
@@ -34,8 +34,8 @@
 				<svelte:fragment slot="h3">What key features can Maigoro offer?</svelte:fragment>
 				<p class="text-gray-500 dark:text-gray-400">
 					Translation Services – Snap a photo or input text to instantly translate into multiple languages.<br>
-Cultural Significance and Context – Get detailed insights about cultural artifacts, symbols, or phrases to enhance understanding.<br>
-Guided Wisdom – Use Maigoro as a tool for self-reflection by analyzing situations and offering advice rooted in philosophical or spiritual traditions.
+					Cultural Significance and Context – Get detailed insights about cultural artifacts, symbols, or phrases to enhance understanding.<br>
+					Guided Wisdom – Use Maigoro as a tool for self-reflection by analyzing situations and offering advice rooted in philosophical or spiritual traditions.
 				</p>
 			</FaqItem>
 			<FaqItem>
