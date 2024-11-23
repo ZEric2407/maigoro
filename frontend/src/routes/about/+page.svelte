0bd62a713494564e1aeba03d88b20d598d9240da
Frank Lin <165003762+SteamyFuton@users.noreply.github.com>
2024-11-23 14:15:25 -0500

    update faq/ maigoro definition

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -13,20 +13,25 @@
 
 <Section name="faq" sectionClass="py-0 [&>div]:py-8">
 	<h2 class="mb-8 text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white">
-		Frequently asked questions
+		Frequently Asked Questions (FAQs)
 	</h2>
 	<Faq>
 		<div>
 			<FaqItem>
 				<svelte:fragment slot="h3">What does Maigorou mean?</svelte:fragment>
 				<p class="text-gray-500 dark:text-gray-400">
-					ITEM 1
+					Maigoro ( /ˈmaɪ-goʊ-roʊ/, also MY-go-roh ) is the 
+					personal guru that spiritually guides one, connecting 
+					one's mind with wisdom. It may offer help to one's inner 
+					and outer journey by giving a deeper insight to the world. 
+					Being able to help one in a moment of doubt, maigoro will 
+					allow one to reach a deeper level of understanding of their environment.
 				</p>
 			</FaqItem>
 			<FaqItem>
 				<svelte:fragment slot="h3">Can I upload pictures directly from my app?</svelte:fragment>
 				<p class="text-gray-500 dark:text-gray-400">
-					ITEM 2
+					yessirrrrr
 				</p>
 			</FaqItem>
 		</div>
