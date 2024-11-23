f6de38958696e8367bf6b2ef3cea20cc5f9dc9c5
Frank Lin <165003762+SteamyFuton@users.noreply.github.com>
2024-11-23 14:47:06 -0500

    Update +page.svelte

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -12,26 +12,46 @@
 </p>
 
 <Section name="faq" sectionClass="py-0 [&>div]:py-8">
-	<h2 class="mb-8 text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white">
-		Frequently Asked Questions (FAQs)
+	<h2 class="mb-8 text-4x1 font-extrabold tracking-tight text-gray-900 dark:text-white">
+		Frequently Asked Questions (FAQ)
 	</h2>
 	<Faq>
 		<div>
 			<FaqItem>
-				<svelte:fragment slot="h3">What does Maigorou mean?</svelte:fragment>
+				<svelte:fragment slot="h3">What does "Maigorou" mean?</svelte:fragment>
 				<p class="text-gray-500 dark:text-gray-400">
 					Maigoro ( /ˈmaɪ-goʊ-roʊ/, also MY-go-roh ) is the 
 					personal guru that spiritually guides one, connecting 
-					one's mind with wisdom. It may offer help to one's inner 
+					one's mind with wisdom and offering help to one's inner 
 					and outer journey by giving a deeper insight to the world. 
 					Being able to help one in a moment of doubt, maigoro will 
 					allow one to reach a deeper level of understanding of their environment.
 				</p>
 			</FaqItem>
 			<FaqItem>
+				<svelte:fragment slot="h3">What key features can Maigoro offer?</svelte:fragment>
+				<p class="text-gray-500 dark:text-gray-400">
+					translation, cultural significance, cultural context, 
+				</p>
+			</FaqItem>
+			<FaqItem>
+				<svelte:fragment slot="h3">Who can use Maigoro?</svelte:fragment>
+				<p class="text-gray-500 dark:text-gray-400">
+					Anyone with a camera can use Maigoro!
+				</p>
+			</FaqItem>
+			<FaqItem>
 				<svelte:fragment slot="h3">Can I upload pictures directly from my app?</svelte:fragment>
 				<p class="text-gray-500 dark:text-gray-400">
-					yessirrrrr
+					Yes, you may also import a picture of your choice.
+				</p>
+			</FaqItem>
+			<FaqItem>
+				<svelte:fragment slot="h3">What's to come?</svelte:fragment>
+				<p class="text-gray-500 dark:text-gray-400">
+					We are currently developing hardware that people with disabilities can use such as 
+					wearable devices that will scan the surroundings of one's environment and communicate it
+					to the user.	
 				</p>
 			</FaqItem>
 		</div>
