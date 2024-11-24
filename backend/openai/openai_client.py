c87bcd9507fe7b50271046a1f4258a24351daff8
Kaos1 <116521023+Kaos1@users.noreply.github.com>
2024-11-23 20:18:23 -0500

    update openai_client.py

--- backend/openai/openai_client.py
+++ backend/openai/openai_client.py
@@ -1,5 +1,7 @@
 import os
 import dotenv
+import ast
+from model import text_translation
 dotenv.load_dotenv()  # Load environment variables from .env
 
 from openai import OpenAI
@@ -8,7 +10,46 @@ client = OpenAI(
     api_key = os.environ.get("OPENAI_API_KEY")
 )
 
-completion = client.chat.completions.create(
+original_text = text_translation.original_text
+target_language = text_translation.target_language
+
+filter = "Given the following text:"+ original_text +", identify the most culturally significant words. Provide them as a JSON array of strings."
+
+filter_response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": filter}
+    ]
+)
+
+filtered_words = filter_response.choices[0].message['content']
+try:
+    significant_words = ast.literal_eval(filtered_words)
+except Exception as e:
+    print(f"Failed to parse the response: {filtered_words}")
+    significant_words = []
+
+cultural_significance_list = []
+
+for i in significant_words:
+    cultural_significance = "Given the following word:" + significant_words[i] + ", give the cultural significance of that word. Provide a consise explanation"
+
+    cultural_response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": cultural_significance}
+        ]
+    )
+
+    cultural_significance_list.append(cultural_response.choices[0].message['content'])
+
+for j in significant_words:
+    cultural_answer = j + ": " + cultural_significance_list[j]
+    print(cultural_answer)
+
+"""completion = client.chat.completions.create(
     model="gpt-4o-mini",
     messages=[
         {"role": "system", "content": "You are a helpful assistant."},
@@ -19,4 +60,4 @@ completion = client.chat.completions.create(
     ]
 )
 
-print(completion.choices[0].message)
+print(completion.choices[0].message)"""
